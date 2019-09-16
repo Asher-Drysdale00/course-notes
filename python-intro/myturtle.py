4919cbed5aa4ad248d343bf6569e950c0cd53fa4
Ash <W0410188@campus.nscc.ca>
2019-09-16 10:08:29 -0300

    first turle program

---
+++ python-intro/myturtle.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.color("red")
+turtle.forward(100)
+turtle.left(135)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(135)
+turtle.forward(141)
+turtle.right(45)
+turtle.forward(100)
